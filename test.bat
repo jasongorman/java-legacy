d69fec9bd1ace7235252107e3fd894381f2e3564
Jason Gorman <jason.gorman@codemanship.com>
2020-06-27 10:37:22 +0100

    Added separate .bat for JDK 13 bin

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk-13.0.2\bin\java" -jar "target\java_legacy-1.0-SNAPSHOT-shaded.jar" "tt0096754" "tt0060666" "tt0317303"
+java13 -jar "target\java_legacy-1.0-SNAPSHOT-shaded.jar" "tt0096754" "tt0060666" "tt0317303"
